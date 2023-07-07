c6cf71817a339e721d4c7615d0e3403ca51772bf
Muhammadamin <webhacker0100@gmail.com>
2023-07-07 11:26:40 +0500

    Added main content's in landing components

---
+++ src/components/LineChart.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LineChart = () => {
+    return (
+        <h1>LineChart</h1>
+    )
+}
+
+export default LineChart
---
+++ src/components/NavLink.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NavLink = () => {
+    return (
+        <h1>NavLink</h1>
+    )
+}
+
+export default NavLink
