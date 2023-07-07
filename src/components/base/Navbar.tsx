7089d5ef7c125b62d4a2e84d0c1952ff6ef415f6
Muhammadamin <webhacker0100@gmail.com>
2023-07-07 11:59:47 +0500

    Deleted exces items

--- src/components/base/Navbar.tsx
+++ src/components/base/Navbar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { ReactComponent as Logo } from '../../assets/img/logo/nefa.svg';
 
 const Navbar = () => {
   const [open, setOpen] = useState<Boolean>(false);
@@ -10,11 +11,12 @@ const Navbar = () => {
         <div className="flex flex-col lg:flex-row items-center space-x-4 xl:space-x-8">
           <div className="w-full flex flex-row items-center justify-between py-6">
             <div>
-              <img
-                src={"../../assets/img/logo/nefa.svg"}
+                <Logo />
+              {/* <img
+                src="../../assets/img/logo/nefa.png"
                 className="w-24 xl:w-28"
                 alt="Nefa Logo"
-              />
+              /> */}
             </div>
             <button
               className="rounded-lg lg:hidden focus:outline-none focus:shadow-outline"
@@ -63,56 +65,6 @@ const Navbar = () => {
               <NavLink name="Watchlist" url="#" />
               <NavLink name="NFT" url="#" />
               <NavLink name="Portfolio" url="#" /> */}
-            <li className="relative group">
-              <button
-                className="md:px-4 py-2 text-sm bg-transparent rounded-lg text-[#666666] hover:text-gray-900 focus:outline-none focus:shadow-outline flex items-center"
-                //   @click="dropdownToggler"
-                //   @blur="dropdownToggler"
-              >
-                <span>Products</span>
-                {/* <ChevronUpIcon v-if="dropdownNavbar" :size="16" />
-                  <ChevronDownIcon v-else :size="16" /> */}
-              </button>
-              {/* <transition name="transform-fade-down"> */}
-              <ul
-                // v-if="dropdownNavbar"
-                className="flex lg:absolute flex-col max-w-42 py-1 lg:bg-white rounded-md lg:shadow-md pl-2 lg:pl-0"
-              >
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Exchange
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Wallet
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Explorer
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Charts
-                  </a>
-                </li>
-              </ul>
-              {/* </transition> */}
-            </li>
           </ul>
         </div>
         <div
