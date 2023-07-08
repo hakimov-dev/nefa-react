c57fc70924ac2fdc27cd5da034a9e0ac67859aec
Muhammadamin <webhacker0100@gmail.com>
2023-07-08 15:47:59 +0500

    Added new props

--- src/components/landing/Step.tsx
+++ src/components/landing/Step.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-const Step = (step: any) => {
+const Step = (props: any) => {
   return (
     <div className="max-w-[280px] xl:max-w-[363px] space-y-6 sm:space-y-3 text-center">
       <img
-        src={require(`../../assets/img/getting-started/${step.img}`)}
+        src={require(`../../assets/img/getting-started/${props.step.img}`)}
         className="max-w-[245px] mx-auto"
         alt=""
       />
-      <h3 className="text-xl text-neutral-800 font-semibold">{step.title}</h3>
+     <h3 className="text-xl text-neutral-800 font-semibold">{props.step.title}</h3>
       <p className="text-sm text-gray-700 leading-relaxed">
-        {step.description}
-      </p>
+        {props.step.description}
+      </p> 
     </div>
   );
 };
