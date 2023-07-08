a02afd3d50601f527cad1a7130c2a1037cf6bcba
Muhammadamin <webhacker0100@gmail.com>
2023-07-08 15:50:18 +0500

    Added new section

--- src/App.tsx
+++ src/App.tsx
@@ -534,6 +534,24 @@ function App() {
         </div>
       </div>
     </section>
+    {/* <!-- FAQ section --> */}
+    <section className="w-full my-24">
+      <BaseSection>
+        <div data-aos="fade-right" data-aos-delay="150" className="col-span-12 lg:col-span-6">
+          <div className="w-full">
+            <img src={require('./assets/img/faq.webp')} className="w-full" alt="" />
+          </div>
+        </div>
+        <div data-aos="fade-left" data-aos-delay="150" className="col-span-12 lg:col-span-6 px-4 sm:px-6 mt-8">
+          <span className="text-base text-gradient font-semibold uppercase mb-4 sm:mb-2">Support</span>
+          <h2 className="text-3xl sm:text-4xl font-semibold mb-10 sm:mb-6">Frequently asked questions</h2>
+
+          <ul className="shadow-box">
+            {/* <BaseAccordion v-for="(accordion, index) in accordions" :key="index" :accordion="accordion" /> */}
+          </ul>
+        </div>
+      </BaseSection>
+    </section>
       </DefaultLayout>
     </div>
   );
