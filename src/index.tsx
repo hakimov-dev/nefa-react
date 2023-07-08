3bf05547f7e113253cdf6f80f49f9a14c7ea6e45
Muhammadamin <webhacker0100@gmail.com>
2023-07-08 16:22:39 +0500

    Added aos animation package

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './assets/css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
