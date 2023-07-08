79fb736851101240b962b6be630457334b99377c
Muhammadamin <webhacker0100@gmail.com>
2023-07-08 14:37:08 +0500

    Added new items

--- src/App.tsx
+++ src/App.tsx
@@ -313,6 +313,21 @@ function App() {
             </div>
           </BaseSection>
         </section>
+        {/* <!-- Partners section --> */}
+    <section className="bg-partner relative max-w-full sm:mx-6 my-24 shadow sm:rounded-2xl overflow-hidden">
+      <div className="w-full px-6 sm:px-0 py-16 flex flex-col items-center justify-center space-y-4 text-center">
+        <h3 data-aos="flip-down" className="text-2xl text-neutral-800 font-semibold">Trusted Partners Worldwide</h3>
+        <p data-aos="flip-down" className="paragraph">We're partners with countless major organisations around the globe</p>
+        <div data-aos="fade-up" className="flex flex-wrap items-center justify-center">
+          
+          {/* <LandingPartnerImage
+            v-for="img in ['clever.png', 'diamon-cutts.png', 'swiss-finance.png', 'gambio.png']"
+            :key="img"
+            :img="img"
+          /> */}
+        </div>
+      </div>
+    </section>
       </DefaultLayout>
     </div>
   );
