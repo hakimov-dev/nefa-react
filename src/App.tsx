2b14e9f4f18bb63fa5036d4becb6d8a9bf96cc60
Muhammadamin <webhacker0100@gmail.com>
2023-07-08 15:02:32 +0500

    Update items

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import BaseButton from "./components/base/Button";
 import BaseSection from "./components/base/Section";
 import LandingCryptoStatistic from "./components/landing/CryptoStatistic";
 import LandingExchange from "./components/landing/Exchange";
+import LandingListItem from "./components/landing/ListItem";
 
 function App() {
   const trendings = [
@@ -329,43 +330,126 @@ function App() {
               data-aos="fade-up"
               className="flex flex-wrap items-center justify-center"
             >
-               {['clever.png', 'diamon-cutts.png', 'swiss-finance.png', 'gambio.png'].map(img => {
+              {[
+                "clever.png",
+                "diamon-cutts.png",
+                "swiss-finance.png",
+                "gambio.png",
+              ].map((img) => {
                 return (
-                <img 
-                 src={require(`./assets/img/partner/${img}`)}
-                 className="sm:w-1/2 lg:w-72 mx-auto" alt="" 
-                 />
-                )
-               })}
+                  <img
+                    src={require(`./assets/img/partner/${img}`)}
+                    className="sm:w-1/2 lg:w-72 mx-auto"
+                    alt=""
+                  />
+                );
+              })}
             </div>
           </div>
         </section>
         {/* <!-- Credit card section --> */}
-    <section className="w-full my-36">
-      <BaseSection data-aos="fade-down">
-        <div className="col-span-12 lg:col-span-7">
-          <div className="w-full">
-            <img 
-            src={require('./assets/img/nefa-cc.webp')} 
-            className="w-[95%]" alt="" />
+        <section className="w-full my-36">
+          <BaseSection data-aos="fade-down">
+            <div className="col-span-12 lg:col-span-7">
+              <div className="w-full">
+                <img
+                  src={require("./assets/img/nefa-cc.webp")}
+                  className="w-[95%]"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="col-span-12 lg:col-span-5 space-y-6 px-4 sm:px-6 mt-20">
+              <h2 className="text-4xl font-semibold">
+                Introducing the{" "}
+                <span className="text-header-gradient">NEFA</span> Credit Card
+              </h2>
+              <p className="paragraph">
+                Subject to cardholder and rewards terms which will be available
+                at application.
+              </p>
+              <ul className="space-y-4 sm:space-y-2">
+                <LandingListItem title="Up to 3% back on purchases">
+                  {" "}
+                </LandingListItem>
+                <LandingListItem title="Earn rewards in bitcoin or any crypto on NEFA">
+                  {" "}
+                </LandingListItem>
+                <LandingListItem title="No annual fee"> </LandingListItem>
+              </ul>
+              <BaseButton style="w-full sm:max-w-[240px] px-10 py-4 bg-inherit text-gradient border border-[#0c66ee] text-base">
+                Join the waitlist
+              </BaseButton>
+            </div>
+          </BaseSection>
+        </section>
+        {/* <!-- Advanced trading tools section --> */}
+        <section className="bg-trading-tools relative max-w-full sm:mx-4 my-20 py-16 shadow rounded-2xl overflow-hidden">
+          <div className="relative max-w-screen-xl px-4 sm:px-2 mx-auto grid grid-cols-12 gap-x-6">
+            <div className="col-span-12 lg:col-span-6 sm:hidden">
+              <div className="w-full sm:mt-20 xl:mt-0">
+                <img
+                  src={require(`./assets/img/advanced-trading-tools.webp`)}
+                  className="w-full"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div
+              data-aos="fade-right"
+              className="col-span-12 lg:col-span-6 space-y-8 sm:space-y-6 px-4 sm:px-6 mt-8"
+            >
+              <h2 className="text-4xl font-semibold">
+                Advanced Trading{" "}
+                <span className="text-header-gradient">Tools</span>
+              </h2>
+              <div className="space-y-2">
+                <h4 className="text-lg font-medium">
+                  Professional Access, Non-stop Availability
+                </h4>
+                <p className="paragraph text-sm xl:text-base">
+                  We provide premium access to crypto trading for both
+                  individuals and institutions through high liquidity, reliable
+                  order execution and constant uptime.
+                </p>
+              </div>
+              <div className="space-y-2">
+                <h4 className="text-lg font-medium">
+                  A Range of Powerful Apis
+                </h4>
+                <p className="paragraph text-sm xl:text-base">
+                  Set up your own trading interface or deploy your algorithmic
+                  strategy with our high-performance FIX and HTTP APIs. Connect
+                  to our WebSocket for real-time data streaming.
+                </p>
+              </div>
+              <div className="space-y-2">
+                <h4 className="text-lg font-medium">Customer Support</h4>
+                <p className="paragraph text-sm xl:text-base">
+                  Premium 24/7 support available to all customers worldwide by
+                  phone or email. Dedicated account managers for partners.
+                </p>
+              </div>
+              <div className="flex flex-col sm:flex-row">
+                <BaseButton style="px-10 py-4 bg-inherit text-gradient border border-[#4A8FF6] text-base">
+                  Get Started
+                </BaseButton>
+                <BaseButton style="bg-inherit text-[#4A8FF6] px-10 py-4 text-center underline rounded-full hover:shadow-none">
+                  Learn More
+                </BaseButton>
+              </div>
+            </div>
+            <div data-aos="fade-left" className="col-span-12 lg:col-span-6 hidden sm:block">
+              <div className="w-full sm:mt-20 xl:mt-0">
+                <img
+                  src={require(`./assets/img/advanced-trading-tools.webp`)}
+                  className="w-full"
+                  alt=""
+                />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="col-span-12 lg:col-span-5 space-y-6 px-4 sm:px-6 mt-20">
-          <h2 className="text-4xl font-semibold">
-            Introducing the <span className="text-header-gradient">NEFA</span> Credit Card
-          </h2>
-          <p className="paragraph">Subject to cardholder and rewards terms which will be available at application.</p>
-          <ul className="space-y-4 sm:space-y-2">
-            {/* <LandingListItem title="Up to 3% back on purchases" />
-            <LandingListItem title="Earn rewards in bitcoin or any crypto on NEFA" />
-            <LandingListItem title="No annual fee" /> */}
-          </ul>
-          <BaseButton
-            style="w-full sm:max-w-[240px] px-10 py-4 bg-inherit text-gradient border border-[#0c66ee] text-base"
-            >Join the waitlist</BaseButton>
-        </div>
-      </BaseSection>
-    </section>
+        </section>
       </DefaultLayout>
     </div>
   );
