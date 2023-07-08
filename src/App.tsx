3f8b289a04252d78b1b158b20057d8b42e608d8c
Muhammadamin <webhacker0100@gmail.com>
2023-07-08 15:51:11 +0500

    Added new variable

--- src/App.tsx
+++ src/App.tsx
@@ -157,6 +157,29 @@ function App() {
     },
   ]
 
+  const accordions = [
+    {
+      title: 'Why should I choose NEFA?',
+      description:
+        "We're industry pioneers, having been in the cryptocurrency industry since 2016. We've facilitated more than 21 billion USD worth of transactions on our exchange for customers in over 40 countries. Today, we're trusted by over 8 million customers around the world and have received praise for our easy-to-use app, secure wallet, and range of features.",
+    },
+    {
+      title: 'How secure is NEFA?',
+      description:
+        "We're industry pioneers, having been in the cryptocurrency industry since 2016. We've facilitated more than 21 billion USD worth of transactions on our exchange for customers in over 40 countries. Today, we're trusted by over million customers around the world and have received praise for our easy-to-use app, secure wallet, and range of features.",
+    },
+    {
+      title: 'Do I have to buy a whole Bitcoin?',
+      description:
+        "We're industry pioneers, having been in the cryptocurrency industry since 2016. We've facilitated more than 21 billion USD worth of transactions on our exchange for customers in over 40 countries. Today, we're trusted by over million customers around the world and have received praise for our easy-to-use app, secure wallet, and range of features.",
+    },
+    {
+      title: 'How do I actually buy Bitcoin?',
+      description:
+        "We're industry pioneers, having been in the cryptocurrency industry since 2016. We've facilitated more than 21 billion USD worth of transactions on our exchange for customers in over 40 countries. Today, we're trusted by over million customers around the world and have received praise for our easy-to-use app, secure wallet, and range of features.",
+    },
+  ]
+
   return (
     <div className="App">
       <DefaultLayout>
