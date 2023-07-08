37153e2c9466a4005433bae0e79e55070673a089
Muhammadamin <webhacker0100@gmail.com>
2023-07-08 14:37:25 +0500

    Added new section

--- src/App.tsx
+++ src/App.tsx
@@ -286,13 +286,13 @@ function App() {
                   exchanges={currencies}
                 />
                 <LandingExchange
-              title="Get"
-              name="get"
-              type="number"
-              defaultValue="0.10901"
-              exchangeSelected={cryptoSelected}
-              exchanges={cryptocurrencies}
-            />
+                  title="Get"
+                  name="get"
+                  type="number"
+                  defaultValue="0.10901"
+                  exchangeSelected={cryptoSelected}
+                  exchanges={cryptocurrencies}
+                />
                 <BaseButton style="w-full px-5 py-4 bg-blue-gradient text-white text-base font-medium">
                   Buy Now
                 </BaseButton>
@@ -314,20 +314,29 @@ function App() {
           </BaseSection>
         </section>
         {/* <!-- Partners section --> */}
-    <section className="bg-partner relative max-w-full sm:mx-6 my-24 shadow sm:rounded-2xl overflow-hidden">
-      <div className="w-full px-6 sm:px-0 py-16 flex flex-col items-center justify-center space-y-4 text-center">
-        <h3 data-aos="flip-down" className="text-2xl text-neutral-800 font-semibold">Trusted Partners Worldwide</h3>
-        <p data-aos="flip-down" className="paragraph">We're partners with countless major organisations around the globe</p>
-        <div data-aos="fade-up" className="flex flex-wrap items-center justify-center">
-          
-          {/* <LandingPartnerImage
+        <section className="bg-partner relative max-w-full sm:mx-6 my-24 shadow sm:rounded-2xl overflow-hidden">
+          <div className="w-full px-6 sm:px-0 py-16 flex flex-col items-center justify-center space-y-4 text-center">
+            <h3
+              data-aos="flip-down"
+              className="text-2xl text-neutral-800 font-semibold"
+            >
+              Trusted Partners Worldwide
+            </h3>
+            <p data-aos="flip-down" className="paragraph">
+              We're partners with countless major organisations around the globe
+            </p>
+            <div
+              data-aos="fade-up"
+              className="flex flex-wrap items-center justify-center"
+            >
+              {/* <LandingPartnerImage
             v-for="img in ['clever.png', 'diamon-cutts.png', 'swiss-finance.png', 'gambio.png']"
             :key="img"
             :img="img"
           /> */}
-        </div>
-      </div>
-    </section>
+            </div>
+          </div>
+        </section>
       </DefaultLayout>
     </div>
   );
