ace6a0516eed898b5a5a906f17147d939b3a9e4d
Muhammadamin <webhacker0100@gmail.com>
2023-07-08 14:48:14 +0500

    Added main content

---
+++ src/components/landing/ListItem.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from "react";
+
+interface propsTypes {
+  children: ReactNode;
+  title: String;
+}
+
+const ListItem: React.FC<propsTypes> = ({ children, title }) => {
+  return (
+    <li className="space-y-2">
+      <div className="flex items-center space-x-2">
+        {/* <CheckCircleIcon :size="20" class="text-[#0c66ee]" /> */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-4 text-[#0c66ee]"
+        >
+          <path
+            fillRule="evenodd"
+            d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+            clipRule="evenodd"
+          />
+        </svg>
+
+        <span>{title}</span>
+      </div>
+      {children}
+    </li>
+  );
+};
+
+export default ListItem;
