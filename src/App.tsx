2a683c1a90a82b8bb4afd3e01f64355e041cf636
Muhammadamin <webhacker0100@gmail.com>
2023-07-08 16:25:09 +0500

    Added aos animation in crypto stat section

--- src/App.tsx
+++ src/App.tsx
@@ -279,7 +279,7 @@ function App() {
           </BaseSection>
         </section>
         {/* <!-- Crypto statistic section --> */}
-        <section className="max-w-screen-xl mx-2 sm:mx-auto px-4 sm:px-6 lg:px-0 py-6 pb-20 sm:py-8 rounded-[2.25rem] sm:rounded-xl bg-white shadow-lg sm:shadow-md transform lg:-translate-y-12">
+        <section data-aos="fade-up" className="max-w-screen-xl mx-2 sm:mx-auto px-4 sm:px-6 lg:px-0 py-6 pb-20 sm:py-8 rounded-[2.25rem] sm:rounded-xl bg-white shadow-lg sm:shadow-md transform lg:-translate-y-12">
           <div className="w-full flex flex-col lg:flex-row items-center justify-center">
             <LandingCryptoStatistic
               title="🔥 Trending"
